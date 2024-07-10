837b9e0b94021eb24804266d6a910f5814aa0a0a
hirakawataiki <t180sx4116@docomo.ne.jp>
2024-07-10 15:05:28 +0900

    画像の自動移動

--- public/scripts/movePicture.js
+++ public/scripts/movePicture.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var startX, startY, initialX, initialY;
 
   var touchStartHandler = function(e) {
-    if (e.touches.length === 1) { // シングルタッチのみを処理お
+    if (e.touches.length === 1) { // シングルタッチのみを処理
       isDragging = true;
       startX = e.touches[0].clientX;
       startY = e.touches[0].clientY;
@@ -29,6 +29,21 @@ document.addEventListener('DOMContentLoaded', function() {
     isDragging = false;
     catImage.style.cursor = 'grab';
   };
+
+  // 画像を左端から200px右に動かす
+  catImage.animate(
+    // 途中の状態を表す配列
+    [
+      { transform: 'translateX(0)'}, // 開始時の状態（左端）
+      { transform: 'translateX(200px)' } // 終了時の状態（左端から200pxの位置）
+    ], 
+    // タイミングに関する設定
+    {
+      fill: 'backwards', // 再生前後の状態（再生前、開始時の状態を適用）
+      duration: 1000, // 再生時間（1000ミリ秒）
+      iterations: Infinity,  // アニメーションの繰り返し回数（ずっと繰り返す）
+    },
+  );
 
   catImage.addEventListener('touchstart', touchStartHandler);
   document.addEventListener('touchmove', touchMoveHandler);
