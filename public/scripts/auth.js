f45675894ec6c2a2f6dc4a73f6879855daa01db6
shoei03 <fujin.raijin.1415@gmail.com>
2024-07-13 15:18:26 +0900

    ログイン中とログアウト中でボタンの表示と非表示を切り替えた。

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -2,49 +2,53 @@ import { onAuthStateChanged, signOut, signInWithEmailAndPassword } from "https:/
 import { auth } from "./firebase-config.js";
 
 // ログイン機能
-const authButton = document.getElementById('login-button');
-if (authButton) {
-  authButton.addEventListener('click', () => {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+const loginButton = document.getElementById('login-button');
+if (loginButton) {
+  loginButton.addEventListener('click', async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // ログイン成功
-        console.log("ログイン成功!");
-        window.location.href = "index.html";
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ログイン失敗時のエラー処理
-        alert("エラー: " + errorMessage);
-      });
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      // ログイン成功
+      console.log("ログイン成功!");
+      window.location.href = "index.html";
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ログイン失敗時のエラー処理
+      alert(`エラー: ${errorMessage}`);
+    }
   });
 }
 
 // ログアウト機能
 const logoutButton = document.getElementById('logout-button');
 if (logoutButton) {
-  logoutButton.addEventListener('click', () => {
-    signOut(auth).then(() => {
+  logoutButton.addEventListener('click', async () => {
+    try {
+      await signOut(auth);
       // ログアウト成功
       console.log("ログアウトしました");
       window.location.href = "login.html"; // ログインページにリダイレクト
-    }).catch((error) => {
+    } catch (error) {
       // エラー処理
       console.error("ログアウトエラー", error);
-    });
+    }
   });
 }
 
 // ユーザーのログイン状態の監視
-document.addEventListener('DOMContentLoaded', () => {
+const authContainer = document.querySelectorAll('.auth-container');
+document.addEventListener('DOMContentLoaded', async () => {
   onAuthStateChanged(auth, (user) => {
     if (user) {
       // ユーザーがログインしている場合の処理
-      console.log("ユーザーがログインしています:", user.email);
+      console.log(`ユーザーがログインしています: ${user.email}`);
       logoutButton.style.display = 'block'; // ログアウトボタンを表示
+      authContainer.forEach((container) => {
+        container.style.display = 'none'; // ログインフォームを非表示
+      });
     } else {
       // ユーザーがログアウトしている場合の処理
       console.log("ユーザーがログアウトしています。");
